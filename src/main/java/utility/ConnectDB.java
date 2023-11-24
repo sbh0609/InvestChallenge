c34764414c0e6ea6607d72cb511e785b17a4e1e1
Blu2ming <yeox84@naver.com>
2023-11-25 02:23:05 +0900

    로그인, 회원가입 기능 추가

--- src/main/java/utility/ConnectDB.java
+++ src/main/java/utility/ConnectDB.java
@@ -3,10 +3,13 @@ package utility;
 
 import java.sql.*;
 
+import utility.userVO;
+
 public class ConnectDB {
 	
 	Connection conn = null ;
 	PreparedStatement pstmt = null;
+	ResultSet rs = null;
 	
 	//환경변수 설정
 	String dbusername = System.getenv("DB_NAME");
@@ -14,19 +17,26 @@ public class ConnectDB {
 	
 	
 	String jdbc_driver = "com.mysql.cj.jdbc.Driver";
-	String jdbc_url = "jdbc:mysql://localhost/jspdb?allowPublicKeyRetrieval=true&useUnicode=true&characterEncoding=utf8&useSSL=false&serverTimezone=UTC";
+	String jdbc_url = "jdbc:mysql://localhost/investchallenge?allowPublicKeyRetrieval=true&useUnicode=true&characterEncoding=utf8&useSSL=false&serverTimezone=UTC";
 	
 	public void connect() {
 		try {
 			Class.forName(jdbc_driver);
-			conn = DriverManager.getConnection(jdbc_url,dbusername,dbpassword);
+			conn = DriverManager.getConnection(jdbc_url,"root","passwd");
 		}catch(Exception e) {
 			e.printStackTrace();
 		}
 		
 	}
 	
-	public void disconnect(Connection conn) {
+	public void disconnect() {
+		if (pstmt != null) {
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
         if (conn != null) {
             try {
                 conn.close();
@@ -35,6 +45,53 @@ public class ConnectDB {
             }
         }
     }
+	
+	public int add(userVO user) {
+		connect();
+		String sql = "insert into user values (?, ?, ?)";
+		int result = 0;
+		try {
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, user.getId());
+			pstmt.setString(2, user.getPw());
+			pstmt.setString(3, user.getUsername());
+			result = pstmt.executeUpdate();
+		}
+		catch (SQLException e) {
+			e.printStackTrace();
+		}
+		finally {
+			disconnect();
+		}
+		return result;
+	}
+	
+	public userVO login(String id, String pw) {
+		userVO user = null;
+		connect();
+		String sql = "select * from user where id = ? and pw = ?";
+		try {
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1,  id);
+			pstmt.setString(2,  pw);
+			rs = pstmt.executeQuery();
+			
+			if (rs.next()) {
+				user = new userVO();
+				user.setId(rs.getString("id"));
+				user.setPw(rs.getString("pw"));
+				user.setUsername(rs.getString("username"));
+			}
+		}
+		catch (SQLException e) {
+			e.printStackTrace();
+		}
+		finally {
+			disconnect();
+		}
+		return user;
+	}
+	
 //	public static void main(String[] args) {
 //		ConnectDB db = new ConnectDB();
 //        db.connect();
