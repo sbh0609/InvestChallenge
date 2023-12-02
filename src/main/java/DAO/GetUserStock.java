d0b85ed1accb79709fd06f4f305c055cb2609d95
OneTable146 <onetable146@gmail.com>
2023-12-02 23:56:31 +0900

    평가손익 수익률 평가금액 계산 메소드화
    계산하는거 메소드로 넘김

--- src/main/java/DAO/GetUserStock.java
+++ src/main/java/DAO/GetUserStock.java
@@ -3,11 +3,15 @@ import utility.ConnectDB;
 import java.util.List;
 import java.util.ArrayList;
 import utility.userVO;
+import DAO.UserStock;
+import service.GetapiData;
 
 import java.sql.*;
 
 
 public class GetUserStock {
+	GetstockCode gsc = new GetstockCode();
+	GetapiData gad = new GetapiData();
 	
 	public List<UserStock> getUserStock(String userID) {
 		List<UserStock> userStockList = new ArrayList<>();
@@ -43,4 +47,25 @@ public class GetUserStock {
 		}
 		return userStockList;
 	}
+	 public List<Integer> getStockInfo (UserStock userStock) {
+	    	String stockName = ((UserStock) userStock).getStockName();
+	    	String stockCode = gsc.getStockCode(stockName);
+	    	String realPrice = gad.LiveStockPrice(stockCode);
+	    	int intRealPrice = (realPrice != null) ? Integer.parseInt(realPrice) : 0;
+	    	//평가손익
+	    	int profitLossValuation = (intRealPrice - ((UserStock)userStock).getBuyPrice()) * ((UserStock)userStock).getStockQuantity();
+	    	//수익률
+	    	double rateOfReturn = ((double) profitLossValuation / (((UserStock)userStock).getBuyPrice() * ((UserStock)userStock).getStockQuantity())) * 100;
+	    	//평가금액
+	    	int marketValue = intRealPrice * ((UserStock)userStock).getStockQuantity();
+	    	
+	    	//리스트의 첫인자 intRealPrice 두번째 인자 profitLossValuation 세번째 인자 rateOfReturn 네번째 인자 marketValue
+	    	List<Integer> result = new ArrayList<>();
+	    	result.add(intRealPrice);
+	    	result.add(profitLossValuation);
+	    	result.add((int) rateOfReturn); 
+	    	result.add(marketValue);	
+	    	
+	    	return result;
+	    	}
 }
